405b507c77238167188d8447c6b1a548b4d80eae
Wesley Brooks <z3509569@matht364.maths.unsw.edu.au>
2015-12-18 16:33:16 +1100

    Created derivative functions for tau, beta in both scratch and src directories.

--- scratch/log-det-chol.r
+++ scratch/log-det-chol.r
@@ -45,11 +45,103 @@ DerLogDetChol2 <- function(P, zed) {
 }
 
 
+DerLogDetChol4 <- function(P) {
+  A <- t(chol(P))
+  Ai <- solve(A)
+  r <- ncol(P)
+  D <- matrix(0, r, r)
+
+  for (j in 1:r) {
+    D[j,j] <- sum(Ai[j:r, j]^2) / 2
+
+    indx <- which(1:r > j)
+    for (i in indx) {
+      D[i, j] <- sum(Ai[i:r, i] * Ai[i:r, j])
+    }
+  }
+
+  D
+}
+
+DerLogDetCholTau <- function(P) {
+  L <- t(chol(P))
+  Li <- solve(L)
+  r <- ncol(P)
+  D <- vector()
+
+  for (j in 1:r) {
+    D <- c(D, L[j,j] * sum(Li[j:r,j]^2) / 2)
+  }
+
+  sum(D / diag(L))
+}
+
+
+DerLogDetCholTau2 <- function(P) {
+  L <- t(chol(P))
+  Li <- solve(L)
+  r <- ncol(P)
+  D <- vector()
+
+  for (j in 1:r) {
+    D <- c(D, sum(Li[j:r,j]^2) / 2)
+  }
+
+  sum(D)
+}
+
+
+
+DerLogDetCholBeta2 <- function(l, S, X, beta, u, wt) {
+  Li <- solve(L)
+  r <- ncol(P)
+  p <- ncol(X)
+  n <- nrow(X)
+  D <- matrix(0, p, r)
+
+  mu <- exp(X %*% beta + S %*% u)
+
+
+  for (j in 1:r) {
+    AS <- vector()
+    for (i in 1:n)
+      AS <- c(AS, wt[i] * mu[i] * sum(Li[j, 1:j] * S[i, 1:j])^2)
+
+
+    D[,j] <- apply(X, 2, function(z) sum(z * AS) / 2)
+  }
+
+  rowSums(D)
+}
+
+
+
+
 data(mtcars)
-X <- as.matrix(mtcars)
-P <- t(X) %*% X
+X <- cbind(1, as.matrix(mtcars[2:11]))
+y <- mtcars$mpg
+n <- nrow(X)
+S <- cbind(rnorm(n), rnorm(n), rnorm(n), rnorm(n))
+r <- ncol(S)
+u <- rnorm(r)
+wt <- runif(n)
+tau <- 6
+
+beta <- lm(mpg~., data=mtcars, weights=wt)$coefficients
+
+mu <- as.vector(exp(X%*%beta + S%*%u))
+
+P <- diag(x=rep(tau, r)) + t(S) %*% diag(x=wt*mu) %*% S
 L <- t(chol(P))
 Li <- solve(L)
+
+# Change beta and see change in log determinant of Cholesky P:
+b2 <- beta
+b2[1] <- b2[1] + 1/1000
+m2 <- as.vector(exp(X %*% b2 + S %*% u))
+P2 <- diag(x=rep(tau, r)) + t(S) %*% diag(x=wt*m2) %*% S
+sum(log(diag(chol(P2)))) - sum(log(diag(chol(P))))
+
 
 zed <- matrix(0, ncol(X), ncol(X))
 zed[5,4] <- zed[4,5] <- 1
--- src/DerLogDetChol.cpp
+++ src/DerLogDetChol.cpp
@@ -56,6 +56,122 @@ Eigen::VectorXd DerLogDetCholIndep(const Eigen::VectorXd diagV)
 }
 
 
+
+
+// [[Rcpp::export]]
+Eigen::VectorXd DerLogDetChol3(const Eigen::MatrixXd L)
+{
+  int r = L.cols();
+  int size = r * (r+1) / 2;
+  Eigen::MatrixXd Li = L.inverse();
+  Eigen::VectorXd D(size);
+  D.setConstant(0);
+
+  int indx = 0;
+  for (int j=0; j<r; j++)
+  {
+    for (int k=j; k<r; k++)
+    {
+      D(indx) += pow(Li(k, j), 2) / 2;
+    }
+    indx += 1;
+
+    for (int i=j+1; i<r; i++)
+    {
+      for (int k=i; k<r; k++)
+        D(indx) += Li(k, i) * Li(k, j);
+
+      indx += 1;
+    }
+  }
+
+  return(D);
+}
+
+
+
+// [[Rcpp::export]]
+double DerLogDetCholTau(const Eigen::MatrixXd L)
+{
+  int r = L.cols();
+  Eigen::MatrixXd Li = L.inverse();
+  double result=0;
+
+  int indx = 0;
+  for (int j=0; j<r; j++)
+  {
+    for (int k=j; k<r; k++)
+      result += pow(Li(k, j), 2) / 2;
+  }
+
+  return(result);
+}
+
+//
+//
+// DerLogDetCholBeta <- function(P, S, X, beta, u, wt) {
+//   L <- t(chol(P))
+//   Li <- solve(L)
+//   r <- ncol(P)
+//   p <- ncol(X)
+//   n <- nrow(X)
+//   D <- matrix(0, p, r)
+//
+//   mu <- exp(X %*% beta + S %*% u)
+//
+//
+//   for (j in 1:r) {
+//     AS <- vector()
+//     for (i in 1:n)
+//       AS <- c(AS, wt[i] * mu[i] * sum(Li[j, 1:j] * S[i, 1:j])^2)
+//
+//
+//       D[,j] <- L[j,j] * apply(X, 2, function(z) sum(z * AS) / 2)
+//   }
+//
+//   apply(D, 1, function(z) sum(z / diag(L)))
+// }
+
+
+// [[Rcpp::export]]
+Eigen::VectorXd DerLogDetCholBeta(const Eigen::MatrixXd L, const Eigen::MatrixXd S, const Eigen::MatrixXd X, const Eigen::VectorXd beta, const Eigen::VectorXd u, const Eigen::VectorXd wt)
+{
+  int r = L.cols();
+  int p = X.cols();
+  int n = X.rows();
+
+  Eigen::MatrixXd Li = L.inverse();
+  Eigen::MatrixXd D(p, r);
+  D.setConstant(0);
+
+  Eigen::VectorXd nu(n);
+  nu = X*beta + S*u;
+
+  Eigen::VectorXd result(p);
+
+  for (int j=0; j<r; j++)
+  {
+    Eigen::VectorXd LiS(n);
+    LiS.setConstant(0);
+
+    for (int i=0; i<n; i++)
+    {
+      for (int k=0; k<=j; k++)
+        LiS(i) += Li(j, k) * S(i, k);
+
+      LiS(i) = pow(LiS(i), 2) * wt(i) * exp(nu(i));
+    }
+
+    for (int k=0; k<p; k++)
+      D(k, j) = (LiS.array() * X.col(k).array()).sum() / 2;
+  }
+
+  for (int j=0; j<p; j++)
+    result(j) = D.row(j).sum();
+
+  return(result);
+}
+
 //
 // #----------------------------------
 // # Now want derivative of log determinant of Cholesky factor w.r.t. parameters beta and tau that appear in the original matrix:
