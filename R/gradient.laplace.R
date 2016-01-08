5ad82e697eccdb63c35d09f09e9f29a2dbf848d9
Wesley Brooks <z3509569@matht364.maths.unsw.edu.au>
2016-01-08 18:03:38 +1100

    Laplace approximation isn't working yet

--- R/gradient.laplace.R
+++ R/gradient.laplace.R
@@ -1,12 +1,38 @@
-#-------------------------
-# Calculate the gradient of the marginal log-likelihood
-gradient.laplace <- function(beta, ltau, y, X, S, u, wt, tol, verbose) {
+#' Gradient of the marginal log-likelihood for a Cox process
+#'
+#' Calculate the gradient of the marginal log-likelihood with respect to coefficients beta and log variance component ltau. Uses the Laplace approximation.
+#'
+#'
+gradient.laplace <- function(beta, ltau, y, X, S, wt, tol, verbose) {
   tryCatch( {
     tau <- exp(ltau)
     p <- ncol(X)
-    eta <- X %*% beta + S %*% u
-    mu <- exp(eta)
     r <- ncol(S)
+    eta <- as.vector(X %*% beta)
+    mu <- exp(eta)
+
+    if (verbose) cat("Maximizing joint likelihood for u:")
+    pseudoCovar <- rbind(S, sqrt(tau/2) * diag(ncol(S)))
+    converged <- FALSE
+    norm.old <- Inf
+    while(!converged) {
+      z <- as.vector(eta - X%*%beta + (y - mu) / mu)
+      pseudodata <- c(z, rep(0, r))
+      pois <- lsfit(x=pseudoCovar, y=pseudodata, intercept=FALSE, wt=c(wt*mu, rep(1, r)))
+
+      eta <- as.vector(X %*% beta + S %*% pois$coefficients)
+      mu <- exp(eta)
+
+      norm.new <- sum(pois$coefficients^2)
+      if (abs(norm.new - norm.old) < tol * (norm.old + tol)) converged <- TRUE
+      norm.old <- norm.new
+      if (verbose) cat(".")
+    }
+    u <- pois$coefficients
+    if (verbose) cat("done!\n")
+
+    eta <- as.vector(X %*% beta + S %*% u)
+    mu <- exp(eta)
 
     # Calculate the Hessian for the u vector:
     H <- as.matrix(t(S) %*% Diagonal(x=wt * mu) %*% S)
@@ -16,27 +42,29 @@ gradient.laplace <- function(beta, ltau, y, X, S, u, wt, tol, verbose) {
     # Compute the Cholesky decomposition of the Hessian
     L <- chol(H)
     L <- t(L)
-    deriv <- DerLogDetChol(L)
+    # deriv <- DerLogDetCholBeta(L, S, X, beta, u, wt)
 
-    # Rate of change of H w.r.t. beta
-    DH.Dbeta <- array(data=0, dim=c(r,r,p))
-    for (j in 1:p) {
-      # Rate of change of H w.r.t. beta[j]:
-      DH.Dbeta[,,j] <- t(S) %*% sweep(S, 1, as.vector(mu*wt*X[,j]), '*')
-    }
+#     # Rate of change of H w.r.t. beta
+#     DH.Dbeta <- array(data=0, dim=c(r,r,p))
+#     for (j in 1:p) {
+#       # Rate of change of H w.r.t. beta[j]:
+#       DH.Dbeta[,,j] <- t(S) %*% sweep(S, 1, as.vector(mu*wt*X[,j]), '*')
+#     }
+#
+#     # Rate of change of log-det-cholesky factor w.r.t. beta.
+#     D2 <- rep(0, p)
+#     for (j in 1:p) {
+#       # Rate of change of log-det-cholesky factor w.r.t. beta[j]
+#       D2[j] <- sum(deriv * DH.Dbeta[,,j])
+#     }
 
-    # Rate of change of log-det-cholesky factor w.r.t. beta.
-    D2 <- rep(0, p)
-    for (j in 1:p) {
-      # Rate of change of log-det-cholesky factor w.r.t. beta[j]
-      D2[j] <- sum(deriv * DH.Dbeta[,,j])
-    }
+    D2 <- DerLogDetCholBeta(L, S, X, beta, u, wt)
 
     # Rate of change of log-det-cholesky factor w.r.t. tau
-    Dtau <- sum(diag(deriv))
+    # Dtau <- sum(diag(deriv))
+    Dtau <- DerLogDetCholTau(L) * tau
 
-
-    c(as.vector(t(X) %*% Diagonal(x=wt) %*% (y - exp(eta)) - D2[1:p]), -tau*sum(u^2)/2 + r/2 - Dtau)
+    -c(as.vector(t(X) %*% Diagonal(x=wt) %*% (y - exp(eta)) - D2[1:p]), -tau*sum(u^2)/2 + r/2 - Dtau)
   }, error=function(e) return(NA)
   )
 }
