76b682bd1b46e1442bb7a45a41e0ba25fb354432
Wesley Brooks <z3509569@matht364.maths.unsw.edu.au>
2016-01-08 17:58:41 +1100

    Fixing gradient calculations

--- R/score.variational.independent.R
+++ R/score.variational.independent.R
@@ -1,11 +1,11 @@
 #--------------------------
 # Gradient of the lower likelihood bound w.r.t. V:
-score.diagV <- function(log.diagV, M, ltau, y, X, S, beta, wt) {
+score.diagV <- function(logV, M, ltau, y, X, S, beta, wt) {
   #grad <- as.vector(colSums(sweep(S^2, 1, wt * mu * v / 2, '*')))
   eta <- as.vector(X %*% beta + S %*% M)
   mu <- exp(eta)
   tau <- exp(ltau)
-  diagV <- exp(log.diagV)
+  diagV <- exp(logV)
 
   v <- exp(VariationalVarIndep(diagV, S) / 2)
 
@@ -15,7 +15,34 @@ score.diagV <- function(log.diagV, M, ltau, y, X, S, beta, wt) {
   #grad.logV <- grad.logV + DerLogDetCholIndep(diagV)
   grad.logV <- grad.logV + 1/2
 
-  grad.logV / 2
+  grad.logV
+}
+
+
+
+#--------------------------
+# Gradient of the lower likelihood bound w.r.t. V:
+score.variational <- function(M, logV, ltau, y, X, S, beta, wt) {
+  log.diagV <- logV
+
+  #grad <- as.vector(colSums(sweep(S^2, 1, wt * mu * v / 2, '*')))
+  eta <- as.vector(X %*% beta + S %*% M)
+  mu <- exp(eta)
+  tau <- exp(ltau)
+  diagV <- exp(log.diagV)
+
+  v <- exp(VariationalVarIndep(diagV, S) / 2)
+
+  # Gradient w.r.t. M:
+  grad.M <- as.vector(t(S) %*% Diagonal(x=wt) %*% (y - mu*v)) - tau*M
+
+  grad.logV <- -as.vector(t(S^2) %*% as.matrix(wt * mu * v))*diagV / 2
+  #grad.logV <- grad.logV + 1
+  grad.logV <- grad.logV - tau*diagV / 2 # D_V
+  #grad.logV <- grad.logV + DerLogDetCholIndep(diagV)
+  grad.logV <- grad.logV + 1/2
+
+  c(grad.M, grad.logV)
 }
 
 
