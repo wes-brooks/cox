ade2c5ba4f1ada2afb76862f4c38ac210f60e358
Wesley Brooks <z3509569@matht364.maths.unsw.edu.au>
2016-01-08 18:00:34 +1100

    Exploratory as I work on the Laplace approximation

--- R/cox.laplace.R
+++ R/cox.laplace.R
@@ -21,56 +21,62 @@ cox.laplace <- function(y, X, S, wt, beta.start, tau.start=100, tol=sqrt(.Machin
   r <- ncol(S)
   p <- ncol(X)
   n <- nrow(X)
+  par <- c(beta, ltau)
 
   # Estimate the regression coefficients
   #res <- optim(beta, fn=marginal.laplace, gr=gradient.laplace, y=dat$resp, X=X, S=S, u=u, ltau=ltau, wt=wt, L=L, method="BFGS")
   # res <- optim(c(beta, ltau), fn=marginal.laplace, gr=gradient.laplace, y=y, X=X, S=S, u=u, wt=wt, method="BFGS")
   # res <- optim(par=ltau, fn=marginal.tau, beta=beta, y=y, X=X, S=S, u=u, wt=wt, method="BFGS")
   # res <- optim(par=c(beta, ltau), fn=marginal.joint, y=y, X=X, S=S, wt=wt, verbose=verbose, method="BFGS")
-  res <- optim(ltau, fn=marginal.ltau, y=y, X=X, S=S, beta=beta, wt=wt, verbose=verbose, tol=tol)
+  #res <- optim(ltau, fn=marginal.ltau, y=y, X=X, S=S, beta=beta, wt=wt, verbose=verbose, tol=tol)
+  #res <- optim(par, fn=marginal.joint, gr=gradient.joint, y=y, X=X, S=S, wt=wt, verbose=verbose, tol=tol, method="BFGS")
+  res <- optim(par, fn=marginal.joint, y=y, X=X, S=S, wt=wt, verbose=verbose, tol=tol, method="BFGS")
+  beta <- res$par[1:p]
+  tau <- tail(res$par, 1)
 
-  ltau <- res$par
-  tau <- exp(ltau)
-
-
-
-  # Compute eta and mu in order to begin the IRLS algorithm
-  eta <- X %*% beta
-  mu <- exp(eta)
-
-  # For fixed ltau, estimate beta and u by IRLS
-  if (verbose) cat("Holding tau fixed to estimate u.")
-  pseudoCovar <- rbind(cbind(X, S), cbind(matrix(0, ncol(S), ncol(X)), sqrt(tau/2) * diag(r)))
-  converged <- FALSE
-  norm.old <- Inf
-  while(!converged) {
-    z <- as.vector(eta + (y - mu) / mu)
-    pseudodata <- c(z, rep(0, r))
-    pois <- lsfit(x=pseudoCovar, y=pseudodata, intercept=FALSE, wt=c(wt*mu, rep(1, r)))
-
-    eta <- as.vector(cbind(X, S) %*% pois$coefficients)
-    mu <- exp(eta)
-
-    norm.new <- sum(pois$coefficients^2)
-    if (abs(norm.new - norm.old) < tol * (norm.old + tol)) converged <- TRUE
-    norm.old <- norm.new
-    if (verbose) cat(".")
-  }
-  u <- tail(pois$coefficients, ncol(S))
-  beta <- pois$coefficients[1:ncol(X)]
-  if (verbose) cat ("done!\n")
-  if (verbose) cat(paste(" ltau=", round(ltau, 3), "\n initial beta=", paste(round(beta, 3), collapse=', '), '\n\n', sep=''))
-
-  if (verbose) cat(paste("Finalized estimation of u, estimating beta.\n", sep=''))
-  res <- optim(beta, fn=marginal.laplace.beta, y=y, ltau=ltau, X=X, S=S, u=u, wt=wt, verbose=verbose, tol=tol)
-  beta <- res$par
+#
+#   ltau <- res$par
+#   tau <- exp(ltau)
+#
+#   # Compute eta and mu in order to begin the IRLS algorithm
+#   eta <- X %*% beta
+#   mu <- exp(eta)
+#
+#   # For fixed ltau, estimate beta and u by IRLS
+#   if (verbose) cat("Holding tau fixed to estimate u.")
+#   pseudoCovar <- rbind(cbind(X, S), cbind(matrix(0, ncol(S), ncol(X)), sqrt(tau/2) * diag(r)))
+#   converged <- FALSE
+#   norm.old <- Inf
+#   while(!converged) {
+#     z <- as.vector(eta + (y - mu) / mu)
+#     pseudodata <- c(z, rep(0, r))
+#     pois <- lsfit(x=pseudoCovar, y=pseudodata, intercept=FALSE, wt=c(wt*mu, rep(1, r)))
+#
+#     eta <- as.vector(cbind(X, S) %*% pois$coefficients)
+#     mu <- exp(eta)
+#
+#     norm.new <- sum(pois$coefficients^2)
+#     if (abs(norm.new - norm.old) < tol * (norm.old + tol)) converged <- TRUE
+#     norm.old <- norm.new
+#     if (verbose) cat(".")
+#   }
+#   u <- tail(pois$coefficients, ncol(S))
+#   beta <- pois$coefficients[1:ncol(X)]
+#   if (verbose) cat ("done!\n")
+#   if (verbose) cat(paste(" ltau=", round(ltau, 3), "\n initial beta=", paste(round(beta, 3), collapse=', '), '\n\n', sep=''))
+#
+#   if (verbose) cat(paste("Finalized estimation of u, estimating beta.\n", sep=''))
+#   res <- optim(beta, fn=marginal.laplace.beta, y=y, ltau=ltau, X=X, S=S, u=u, wt=wt, verbose=verbose, tol=tol)
+#   beta <- res$par
 
 #   if (verbose) cat(paste("Finalized estimation of tau=", round(tau, 2), ", now estimating beta and its covariance matrix.\n", sep=''))
 #   res.fin <- optimHess(c(beta, ltau), fn=marginal.laplace, y=y, X=X, S=S, u=u, wt=wt, verbose=verbose, tol=tol)
 
   # Estimate the Hessian of beta and tau, holding u, beta, and tau fixed at their maximizers
   if (verbose) cat("Finalized estimation of beta and tau, now estimate the hessian.\n")
-  res.fin <- optimHess(par=c(beta, ltau), fn=marginal.joint, gr=gradient.laplace, y=y, X=X, S=S, u=u, wt=wt, tol=tol, verbose=FALSE)
+  #res.fin <- optimHess(par=c(beta, ltau), fn=marginal.joint, gr=gradient.laplace, y=y, X=X, S=S, u=u, wt=wt, tol=tol, verbose=FALSE)
+  #res.fin <- optimHess(par=c(beta, ltau), fn=marginal.joint, gr=gradient.joint, y=y, X=X, S=S, wt=wt, tol=tol, verbose=FALSE)
+  res.fin=2
 
-  list(beta=beta, ltau=ltau, hessian=res.fin, neg.loglik=res$value, u=u)
+  list(beta=beta, ltau=ltau, hessian=res.fin, neg.loglik=res$value, u=u, res=res)
 }
