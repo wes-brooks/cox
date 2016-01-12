db5e2f66a44d43bb179b4a3bc287e0c7f24772c9
--global <--local>
2016-01-12 13:24:46 +1100

    Removed some cruft

--- R/marginal.joint.R
+++ R/marginal.joint.R
@@ -1,13 +1,5 @@
-# #--------------------------
-# # Calculate the (negative) marginal log-likelihod
-# marginal.joint <- function(par, y, X, S, u, wt) {
-#   p <- ncol(X)
-#   beta <- par[1:p]
-#   ltau <- tail(par, 1)
-#   marginal.laplace(beta, ltau, y, X, S, u, wt)
-# }
-#--------------------------
-# Calculate the (negative) marginal log-likelihod
+#' Calculate the (negative) marginal log-likelihod
+#'
 marginal.joint <- function(par, y, X, S, wt, tol, verbose) {
   p <- ncol(X)
   beta <- par[1:p]
@@ -22,6 +14,7 @@ marginal.joint.quick <- function(par, y, X, S, wt, verbose) {
   ltau <- tail(par, 1)
   marginal.laplace.quick(beta, ltau, y, X, S, wt, verbose)
 }
+
 
 gradient.joint <- function(par, y, X, S, wt, tol, verbose) {
   p <- ncol(X)
