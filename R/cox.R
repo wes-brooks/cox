731f5502bed647649253114e0d66bd351f59744d
--global <--local>
2016-06-02 12:01:00 +1000

    Added the sd argument and the 'object' entry of the returned list

--- R/cox.R
+++ R/cox.R
@@ -33,6 +33,7 @@ NULL
 #' @param tol tolerance for judging convergence of the algorithm
 #' @param verbose logical indicating whether to write detailed progress reports to standard output
 #' @param hess logical indicating whether to estimate the Hessian matrix
+#' @param sd logical indicating whether to report the estimated standard errors of the coefficients
 #' @param diagV logical: should the variational approximation to the covariance matrix of the random effects be diagonal?
 #'
 #' @return list of results containing the following elements:
@@ -45,18 +46,22 @@ NULL
 #'
 #' \code{ltau}: estimated precision component for the spatial random effect
 #'
-#' \code{hessian}: estimated hessian matrix for \code{beta}, \code{M}, and \code{ltau} at convergence (estimated by call to \code{optim})
+#' \code{hessian}: estimated hessian matrix for \code{beta}, \code{M}, and \code{ltau} at convergence (estimated by call to \code{optimHess})
+#'
+#' \code{sd}: estimated standard errors of \code{beta}, \code{M}, and \code{ltau} at convergence (estimated by call to \code{sdreport})
+#'
+#' \code{object}: converged model object, returned by \code{nlminb}
 #'
 #' \code{neg.log.lik}: negative of the variational lower bound on the marginal log-likelihood at convergence
 #'
 #' @details Estimate regression coefficients of a spatial Cox process via the variational approximation.
 #'
 #' @export
-cox <- function(y, X, S, wt, beta.start, tau.start=100, tol=sqrt(.Machine$double.eps), verbose=TRUE, hess=TRUE, diagV=FALSE) {
+cox <- function(y, X, S, wt, beta.start, tau.start=100, tol=sqrt(.Machine$double.eps), verbose=TRUE, hess=FALSE, diagV=FALSE, sd=TRUE) {
 
   # Dispatch the function call to the appropriate method: either independent random effects or not.
-  if (diagV) res <- cox.variational.indep(y=y, X=X, S=S, wt=wt, beta.start=beta.start, tau.start=tau.start, tol=tol, verbose=verbose, hess=hess)
-  else   if (diagV) res <- cox.variational(y=y, X=X, S=S, wt=wt, beta.start=beta.start, tau.start=tau.start, tol=tol, verbose=verbose, hess=hess)
+  if (diagV) res <- cox.variational.indep(y=y, X=X, S=S, wt=wt, beta.start=beta.start, tau.start=tau.start, tol=tol, verbose=verbose, hess=hess, sd=sd)
+  else   if (diagV) res <- cox.variational(y=y, X=X, S=S, wt=wt, beta.start=beta.start, tau.start=tau.start, tol=tol, verbose=verbose, hess=hess, sd=sd)
 
   res
 }
